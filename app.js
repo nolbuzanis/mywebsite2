49d7c7533475d52a154c6b95d81ddc455dd962d5
nolbuzanis <nolanbuzanis@gmail.com>
2018-12-27 08:28:47 -0500

    Setup template engine pug

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const path = require('path')
 var app = express()
 const port = 8000
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+// template engine setup
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+// Home route for get request
 app.get('/', function(req, res){
-  res.send('Hello World!')
+  res.render('index', {
+    title: 'Home',
+    header: 'Nolan Buzanis'
+  })
 })
 
 app.listen(port, function(){

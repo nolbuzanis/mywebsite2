1a45d56b809cd304768ac7eb20f4cc2603d43270
nolbuzanis <nolanbuzanis@gmail.com>
2019-01-17 02:02:06 -0500

    Slide animation from main -> projects

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,15 @@
 particlesJS.load('particles-js', './particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+
+var button = document.getElementById('projects-menu').addEventListener('click', showProjects);
+
+function showProjects() {
+  console.log('Button clicked');
+  document.getElementById('projects').classList.toggle('hide-projects');
+  document.getElementById('headercontent').classList.toggle('hide-main');
+
+  document.getElementById('projects').classList.toggle('show');
+  document.getElementById('headercontent').classList.toggle('show');
+}
